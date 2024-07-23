14f31f80601f758e08db30456cc5aad346edbd02
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-07-23 15:31:21 +0800

    fix: fixed some error

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -1,31 +1,31 @@
-// Basic
+#include "ll/api/plugin/RegisterHelper.h"
+#include "mc/server/commands/ServerCommandOrigin.h"
+#include "mc/world/item/components/ComponentItem.h"
+#include "mc/world/item/enchanting/Enchant.h"
+#include "mc/world/item/enchanting/EnchantUtils.h"
+#include <Plugin.h>
+#include <cstdlib>
 #include <direct.h>
 #include <filesystem>
 #include <fstream>
+#include <functional>
 #include <iostream>
-#include <memory>
-#include <string_view>
-#include <utility>
-// Plugin
-#include <Plugin.h>
-
-// Json
-#include <nlohmann/json.hpp>
-
-// LL
-#include "ll/api/plugin/RegisterHelper.h"
 #include <ll/api/command/Command.h>
 #include <ll/api/command/CommandHandle.h>
 #include <ll/api/command/CommandRegistrar.h>
 #include <ll/api/memory/Hook.h>
 #include <ll/api/plugin/NativePlugin.h>
 #include <ll/api/service/Bedrock.h>
-
-
-// MC
+#include <mc/common/Brightness.h>
+#include <mc/enums/MaterialType.h>
+#include <mc/math/Vec3.h>
+#include <mc/server/commands/CommandOrigin.h>
+#include <mc/server/commands/CommandOutput.h>
+#include <mc/server/commands/CommandRegistry.h>
+#include <mc/server/commands/MinecraftCommands.h>
+#include <mc/server/commands/shared/HelpCommand.h>
 #include <mc/world/Minecraft.h>
-// Item
-#include "mc/world/item/components/ComponentItem.h"
+#include <mc/world/actor/player/Player.h>
 #include <mc/world/item/Item.h>
 #include <mc/world/item/ItemDescriptor.h>
 #include <mc/world/item/ItemInstance.h>
@@ -35,46 +35,22 @@
 #include <mc/world/item/registry/ItemRegistryManager.h>
 #include <mc/world/item/registry/ItemRegistryRef.h>
 #include <mc/world/item/registry/ItemStack.h>
-
-// Block
-#include <mc/common/Brightness.h>
-#include <mc/enums/MaterialType.h>
-#include <mc/world/level/block/Block.h>
-#include <mc/world/level/block/VanillaBlockTags.h>
-#include <mc/world/level/block/registry/BlockTypeRegistry.h>
-#include <mc/world/level/block/states/BlockState.h>
-
-
-// Level
 #include <mc/world/level/BlockPalette.h>
 #include <mc/world/level/Level.h>
 #include <mc/world/level/biome/Biome.h>
 #include <mc/world/level/biome/registry/BiomeRegistry.h>
+#include <mc/world/level/block/Block.h>
+#include <mc/world/level/block/VanillaBlockTags.h>
+#include <mc/world/level/block/registry/BlockTypeRegistry.h>
+#include <mc/world/level/block/states/BlockState.h>
 #include <mc/world/level/dimension/Dimension.h>
 #include <mc/world/level/material/Material.h>
-// Color
-#include <mc/deps/core/mce/Color.h>
-#include <mc/world/item/components/ItemColorUtil.h>
-// Math
-#include <mc/math/Vec3.h>
-#include <mc/world/phys/AABB.h>
-// Command
-#include <mc/server/commands/CommandOrigin.h>
-#include <mc/server/commands/CommandOutput.h>
-#include <mc/server/commands/CommandRegistry.h>
-#include <mc/server/commands/MinecraftCommands.h>
-#include <mc/server/commands/shared/HelpCommand.h>
-
-// NBT
-#include <mc/nbt/CompoundTag.h>
-#include <mc/nbt/CompoundTagVariant.h>
-// Network
-#include <mc/deps/core/utility/BinaryStream.h>
-#include <mc/network/ServerNetworkHandler.h>
-#include <mc/network/packet/AvailableCommandsPacket.h>
-#include <mc/network/packet/BiomeDefinitionListPacket.h>
-#include <mc/network/packet/CraftingDataPacket.h>
-#include <mc/network/packet/TextPacket.h>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <string_view>
+#include <unordered_set>
+#include <utility>
 
 
 namespace plugin {
@@ -111,66 +87,72 @@ void writeJSON(const std::string& fileName, json const& tag) {
     out.close();
 }
 
-void dumpItem(json& dest, const Item& item, const ll::Logger& logger) {
+size_t hash(ItemStack const& it) {
+    return std::hash<std::string>()(
+        it.getTypeName() + it.getEffectName() + it.getDescriptionName() + it.getName() + std::to_string(it.getId())
+        + it.getRawNameId()
+    );
+}
+
+
+void dumpItem(json& array, const Item& item, const ll::Logger& logger) {
     logger.info("Dumping item - " + item.getFullItemName());
-    json it = json::object();
 
-    // MetaData - 基础属性
-    it["id"]             = item.getId();             // 物品 ID
-    it["name"]           = item.getFullItemName();   // 全名
-    it["maxDamage"]      = item.getMaxDamage();      // 最大耐久度
-    it["isDamageable"]   = item.isDamageable();      // 是否可损坏
-    it["isFertilizer"]   = item.isFertilizer();      // 是否是肥料
-    it["isThrowable"]    = item.isThrowable();       // 是否可投掷
-    it["isUseable"]      = item.isUseable();         // 是否可使用
-    it["armorValue"]     = item.getArmorValue();     // 护甲值
-    it["attackDamage"]   = item.getAttackDamage();   // 攻击力
-    it["toughnessValue"] = item.getToughnessValue(); // 防御值
-    it["cooldownTime"]   = item.getCooldownTime();   // 冷却时间
-
-    // MetaData - 特殊值
-    json sub = json::array();
+    std::unordered_set<size_t> uniqueAuxs;
     for (int i = 0; i <= 256; ++i) { // 0 ~ 256 穷举特殊值
         try {
             if (item.isValidAuxValue(i)) {
-                const auto st = ItemStack(item, 1, i);
-                json       j  = json::object();
+                auto       _fakeItem = ItemStack(item, 1, i);
+                auto const hashed    = hash(_fakeItem);
+                if (uniqueAuxs.contains(hashed)) continue;
+                uniqueAuxs.insert(hashed);
 
-                // Item - 详细数据
-                j["name"]            = st.getName();            // 物品名称
-                j["typeName"]        = st.getTypeName();        // 物品类型名称
-                j["aux"]             = st.getAuxValue();        // 特殊值
-                j["categoryName"]    = st.getCategoryName();    // 组名称
-                j["customName"]      = st.getCustomName();      // 自定义名称
-                j["descriptionName"] = st.getDescriptionName(); // 描述名称
-                j["effectName"]      = st.getEffectName();      // 效果名称
-                // j["hoverName"]       = st.getHoverName();       // 鼠标悬停名称
+                json j = json::object();
 
-                sub.push_back(j);
+                j["name"]            = _fakeItem.getName();            // 物品名称
+                j["typeName"]        = _fakeItem.getTypeName();        // 物品类型名称
+                j["fullName"]        = item.getFullItemName();         // 全名
+                j["aux"]             = _fakeItem.getAuxValue();        // 特殊值
+                j["id"]              = _fakeItem.getId();              // 物品ID
+                j["categoryName"]    = _fakeItem.getCategoryName();    // 组名称
+                j["descriptionName"] = _fakeItem.getDescriptionName(); // 描述名称
+                j["effectName"]      = _fakeItem.getEffectName();      // 效果名称
+
+                array.push_back(j);
             }
-        } catch (...) {}
+        } catch (...) {
+            logger.error("Failed to dump item - " + item.getFullItemName());
+        }
     }
-
-    it["auxs"] = sub; // 特殊值列表
-    dest.push_back(it);
 }
 
 void dumpItemData(const ll::Logger& logger) {
-    json list    = json::array(); // 全部物品数据
-    int  counter = 0;
+    json datas     = json::object();
+    datas["items"] = json::array();
+
+    // Dump item data
+    auto& list    = datas["items"];
+    int   counter = 0;
     for (const auto& item : ItemRegistryManager::getItemRegistry().getNameToItemMap() | std::views::values) {
         dumpItem(list, *item, logger);
         counter++;
     }
+
+    // i18n
+    datas["tr"] = {
+        {"name",            "名称"      },
+        {"typeName",        "命名空间"},
+        {"fullName",        "全名"      },
+        {"aux",             "特殊值"   },
+        {"id",              "物品ID"    },
+        {"categoryName",    "组名称"   },
+        {"descriptionName", "描述名称"},
+        {"effectName",      "效果名称"}
+    };
+
     logger.info("Dump " + std::to_string(counter) + " item data successfully!");
-
-    // Pack data
-    json dest     = json::object();
-    dest["items"] = list;
-    dest["tr"]    = json::object(); // TODO: 翻译数据
-
-    writeJSON("data/item_data.json", dest);
-    logger.info(R"(Items' data have is to "data/item_data.json")");
+    writeJSON("plugins/DataExtractor/item_data.json", datas);
+    logger.info(R"(Items' data have is to "plugins/DataExtractor/item_data.json")");
 }
 
 
@@ -178,16 +160,22 @@ bool Plugin::enable() {
     auto& logger = getSelf().getLogger();
     logger.info("Enabling...");
 
-    ll::command::CommandRegistrar::getInstance()
-        .getOrCreateCommand("ext", "extract data", CommandPermissionLevel::GameDirectors, CommandFlagValue::None)
-        .overload()
-        .execute([&](CommandOrigin const&, CommandOutput& output) {
-            if (!folderExists("data")) {
-                createFolder(logger, "data");
-            }
-            dumpItemData(logger);
-            output.success("Success!");
-        });
+    if (!folderExists("data")) {
+        createFolder(logger, "data");
+    }
+    dumpItemData(logger);
+    logger.info("Success!");
+
+    CommandContext ctx = CommandContext(
+        "stop",
+        std::make_unique<ServerCommandOrigin>(
+            "Server",
+            ll::service::getLevel()->asServer(),
+            CommandPermissionLevel::Owner,
+            0
+        )
+    );
+    ll::service::getMinecraft()->getCommands().executeCommand(ctx);
 
     return true;
 }
